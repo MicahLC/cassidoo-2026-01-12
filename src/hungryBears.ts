1cb8362681d56adecdc626ea7bfcf42bb654454d
Micah Lapping-Carr <MicahLC@users.noreply.github.com>
2026-01-14 14:43:44 -0600

    Add solution.

--- src/hungryBears.ts
+++ src/hungryBears.ts
@@ -1,5 +1,9 @@
-// define bear type
+type Bear = {
+    name: string,
+    hunger: number
+}
 
-export const hungryBears = (input: any) => {
-    return [];
+export const hungryBears = (bears: Bear[]): String[] => {
+    let average = bears.map(b => Math.max(b.hunger, 0)).reduce((prevVal, curVal) => { return prevVal + curVal }, 0) / (bears.length || 1);
+    return bears.filter(b => b.hunger > average).map(b => b.name).sort((a: string, b: string) => a.localeCompare(b));
 }
